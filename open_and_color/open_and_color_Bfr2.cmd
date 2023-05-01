88ff424e3ad4dff82320748d02f936d9169e9d99
dandas67 <131153028+dandas67@users.noreply.github.com>
2023-05-01 09:49:49 +0300

    Create open_and_color_Bfr2.cmd
    open_and_color_Bfr2 in chimera: all atoms representation colored by heteroatom

---
+++ open_and_color/open_and_color_Bfr2.cmd
@@ -0,0 +1,9 @@
+open #101 C:\Users\danist\OneDrive - post.bgu.ac.il\Ph.D\Bfr\relion\post\284\rerefined\284_Bfr2__real_space_refined_007_moved.pdb
+disp #101
+~ribbon #101
+color #ffff6db60000 ,a ,r #101
+color red ,a #101@o=
+color yellow ,a #101@S=
+sel #101@N=
+~sel #101@N
+color #00006f74c30b ,a sel
