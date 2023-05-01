060b088ee6c62b337b9f1f07d3aeb7a1a91cbfe7
dandas67 <131153028+dandas67@users.noreply.github.com>
2023-05-01 09:47:35 +0300

    Create open_and_color_Bfr1.cmd
    open_and_color_Bfr1 in chimera

---
+++ open_and_color/open_and_color_Bfr1.cmd
@@ -0,0 +1,9 @@
+open #100 C:\Users\danist\OneDrive - post.bgu.ac.il\Ph.D\Bfr\relion\post\284\rerefined\284_Bfr1_real_space_refined_005.pdb
+disp #100
+~ribbon #100
+color #32323232cdcd ,a ,r #100
+color red ,a #100@o=
+color yellow ,a #100@S=
+sel #100@N=
+~sel #100@N
+color #00006f74c30b ,a sel
