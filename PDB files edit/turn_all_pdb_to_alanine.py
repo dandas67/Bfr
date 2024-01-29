007ad24b594fa33c2c6deb2feb4e2f44522d2007
dandas67 <131153028+dandas67@users.noreply.github.com>
2024-01-29 10:20:20 +0200

    Add files via upload

---
+++ PDB files edit/turn_all_pdb_to_alanine.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 21 14:11:51 2023
+
+@author: danist
+"""
+from biopandas.pdb import PandasPdb
+
+def convert_to_polyalanine(input_pdb_file, output_pdb_file):
+    # Load PDB file using BioPandas
+    ppdb = PandasPdb().read_pdb(input_pdb_file)
+
+    # Change all residues to alanine
+    ppdb.df['ATOM']['residue_name'] = 'ALA'
+
+
+ # Filter only the N, O, CA, and CB atoms
+    valid_atoms = ['N','C', 'O', 'CA', 'CB']
+    ppdb.df['ATOM'] = ppdb.df['ATOM'][ppdb.df['ATOM']['atom_name'].isin(valid_atoms)]
+
+    # Update atom serial numbers
+    ppdb.df['ATOM']['atom_number'] = range(1, len(ppdb.df['ATOM']) + 1)
+
+
+    # Update insertion codes
+    ppdb.df['ATOM']['insertion'] = ''
+    
+
+    # Save the new structure to a PDB file using BioPandas
+    ppdb.to_pdb(output_pdb_file, records=['ATOM'])
+    
+input_path= "C:/Users/danist/OneDrive - post.bgu.ac.il/Ph.D/Bfr/relion/post/284/relaxed-structure/" # Change this to your input path
+file_name= "Bfr12-UNK-lwrf_relax284.pdb" # Change this to your input PDB file name
+input_pdb1= input_path+file_name
+
+output_file_name= 'Bfr12_polyala_test.pdb'# Change this to the desired output PDB file name
+output_pdb= input_path+output_file_name
+
+
+if __name__ == "__main__":
+    input_pdb_file = input_pdb1  
+    output_pdb_file = output_pdb  
+
+    convert_to_polyalanine(input_pdb_file, output_pdb_file)
+
+
+
